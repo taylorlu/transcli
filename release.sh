f6b4c7ac793b457a61f181b99d507ed8fdb61eef
zoominlazhu <zoominlazhu@653f6834-aab9-c84e-bf21-5865e8c89758>
2014-02-25 09:12:03 +0000

    Update release script.\n Urgent check in.
    git-svn-id: http://svn01/repositories/r02/Cloud/codecs/transcli@110836 653f6834-aab9-c84e-bf21-5865e8c89758

--- release.sh
+++ release.sh
@@ -2,20 +2,20 @@
 mkdir release_redhat
 cd release_redhat
 mkdir transcli
-cp -f ../bin/transcli ./transcli
-cp -f ../bin/mcnt.xml ./transcli
-cp -rf ../bin/preset ./transcli
-cp -rf ../bin/APPSUBFONTDIR ./transcli
+cp -f ../bin/x64_linux/transcli ./transcli
+cp -f ../bin/x64_linux/mcnt.xml ./transcli
+cp -rf ../bin/x64_linux/preset ./transcli
+cp -rf ../bin/x64_linux/APPSUBFONTDIR ./transcli
 cd transcli
 mkdir codecs tools
 cd ..
-cp -r ../bin/codecs/ffmpeg ./transcli/codecs
-cp -r ../bin/codecs/ffprobe ./transcli/codecs
-cp -rf ../bin/codecs/fonts ./transcli/codecs
+cp -r ../bin/x64_linux/codecs/ffmpeg ./transcli/codecs
+cp -r ../bin/x64_linux/codecs/ffprobe ./transcli/codecs
+cp -rf ../bin/x64_linux/codecs/fonts ./transcli/codecs
 
-cp -r ../bin/tools/mediainfo ./transcli/tools
-cp -r ../bin/tools/mp42ts ./transcli/tools
-cp -r ../bin/tools/MP4Box ./transcli/tools
-cp -r ../bin/tools/yamdi ./transcli/tools
+cp -r ../bin/x64_linux/tools/mediainfo ./transcli/tools
+cp -r ../bin/x64_linux/tools/mp42ts ./transcli/tools
+cp -r ../bin/x64_linux/tools/MP4Box ./transcli/tools
+cp -r ../bin/x64_linux/tools/yamdi ./transcli/tools
 
 tar -zcvf transcli_redhat_`date "+%Y%m%d"`.tar.gz --exclude=.svn --exclude=.git transcli
--- transcli/transcli.pro
+++ transcli/transcli.pro
@@ -15,14 +15,15 @@ DEPENDPATH += $$_PRO_FILE_PWD_/../lib
 SOURCES += clihelper.cpp pplive.cpp transcli.cpp
 
 HEADERS += clihelper.h
-DESTDIR = $$_PRO_FILE_PWD_/../bin
 
 win32 {
     DEPENDPATH += $$_PRO_FILE_PWD_/../lib/ia32
     LIBS += -L$$_PRO_FILE_PWD_/../lib/ia32 -lWSock32 -luser32 -lzlib1
+    DESTDIR = $$_PRO_FILE_PWD_/../bin/win32
 }
 
 unix {
     DEPENDPATH += $$_PRO_FILE_PWD_/../lib/x64_linux
     LIBS += -L$$_PRO_FILE_PWD_/../lib/x64_linux -lc -lzstatic -lpthread -ldl -lirc
+    DESTDIR = $$_PRO_FILE_PWD_/../bin/x64_linux
 }
