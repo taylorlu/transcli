70a1a9167d81ca6db39a81aaee92c21a2a103b34
zoominlazhu <zoominlazhu@653f6834-aab9-c84e-bf21-5865e8c89758>
2014-04-08 03:28:12 +0000

    Update release version, and add hls preset. Review:74896
    git-svn-id: http://svn01/repositories/r02/Cloud/codecs/transcli@114267 653f6834-aab9-c84e-bf21-5865e8c89758

--- include/svnver.h
+++ include/svnver.h
@@ -1,6 +1,6 @@
 #ifndef __SVN_REVISION_H__
 #define __SVN_REVISION_H__
-#define SVN_REVISION "63300"
-#define SVN_DATE  "2014/03/13 15:25:10"
-#define SVN_NOW  "2014/03/13 15:25:40"
+#define SVN_REVISION "63400"
+#define SVN_DATE  "2014/04/08 10:25:10"
+#define SVN_NOW  "2014/04/08 10:25:40"
 #endif // __SVN_REVISION_H__
