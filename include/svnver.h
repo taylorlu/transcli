93613f5b9caa2e206e78eda851d360dbe46eef1f
jinqiudai <jinqiudai@653f6834-aab9-c84e-bf21-5865e8c89758>
2015-04-27 02:03:58 +0000

    update version data Review:102133
    git-svn-id: http://svn01/repositories/r02/Cloud/codecs/transcli@144099 653f6834-aab9-c84e-bf21-5865e8c89758

--- include/svnver.h
+++ include/svnver.h
@@ -1,6 +1,6 @@
 #ifndef __SVN_REVISION_H__
 #define __SVN_REVISION_H__
-#define SVN_REVISION "143698"
-#define SVN_DATE  "2015/04/17 12:30:40"
-#define SVN_NOW  "2015/04/17 12:30:40"
+#define SVN_REVISION "143924"
+#define SVN_DATE  "2015/04/22 15:14:15"
+#define SVN_NOW  "2015/04/27 10:00:00"
 #endif // __SVN_REVISION_H__
