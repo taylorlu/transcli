bf506102aa2706029f6ea43c049af9d57230779c
jinqiudai <jinqiudai@653f6834-aab9-c84e-bf21-5865e8c89758>
2015-06-09 07:01:35 +0000

    modife packet transcli Review:104021
    git-svn-id: http://svn01/repositories/r02/Cloud/codecs/transcli@146082 653f6834-aab9-c84e-bf21-5865e8c89758

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ cp -f ../bin/x64_linux/mcnt.xml ./transcli
 cp -rf ../bin/x64_linux/preset ./transcli
 cp -rf ../bin/x64_linux/APPSUBFONTDIR ./transcli
 cd transcli
-mkdir codecs tools
+mkdir codecs tools muxer 
 cd ..
 cp -r ../bin/x64_linux/codecs/ffmpeg ./transcli/codecs
 cp -r ../bin/x64_linux/codecs/ffprobe ./transcli/codecs
@@ -17,5 +17,6 @@ cp -r ../bin/x64_linux/tools/mediainfo ./transcli/tools
 cp -r ../bin/x64_linux/tools/mp42ts ./transcli/tools
 cp -r ../bin/x64_linux/tools/MP4Box ./transcli/tools
 cp -r ../bin/x64_linux/tools/yamdi ./transcli/tools
+cp -r ../bin/x64_linux/muxer/ffmpeg ./transcli/muxer
 
 tar -zcvf transcli_redhat_`date "+%Y%m%d"`.tar.gz --exclude=.svn --exclude=.git transcli
